e5e8036311445e2da264adfc7ac67e8962275d2f
Gokul915 <kolligokulkrishna@gmail.com>
2024-12-09 06:34:45 -0500

    Add files via upload

---
+++ stat 1.R
@@ -0,0 +1,12 @@
+#' Add Two Numbers
+#'
+#' This function adds two numbers together.
+#'
+#' @param x A numeric value.
+#' @param y A numeric value.
+#'
+#' @return The sum of x and y.
+#' @export
+add_numbers <- function(x, y) {
+  return(x + y)
+}
